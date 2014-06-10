fad128fd41eebc91dcdf83e6711f5ba93c9137e3
unknown <JPAcyber@Latihan35.jpa.gov.my>
2014-06-10 15:49:37 +0800

    day2

---
+++ form4.php
@@ -0,0 +1,22 @@
+<?php
+	$nama = $_POST["nama"];
+	$dob = $_POST["dob"];
+	$jantina = $_POST["jantina"];
+	$membaca = $_POST["membaca"];
+	$memancing = $_POST["memancing"];
+	$memasak = $_POST["memasak"];
+	$lain2 = $_POST["lain2"];
+	$jenis = $_POST["jenis"];
+	$komen = $_POST["komen"];
+	
+	echo "Nama : $nama <br />
+		Tarikh Lahir : $dob <br />
+		Jantina : $jantina <br />
+		Membaca : $membaca <br />
+		Memancing : $memancing <br />
+		Memasak : $memasak <br />
+		Lain-lain : $lain2 <br />
+		Jenis Pekerjaan : $jenis <br />
+		Komen : $komen <br />";
+	
+?>
